1d22c125e2fb1b9fb24fe84c8ebb556cda67de70
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-02-05 12:03:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+s1="10 20 30 40 50"
+list =s1.split()
+print(list)
+s2="10,20,30,40,50"
+list1=s2.split(",")
+print(s2)
+s3="1;2;3;4;5"
+list3=s3.split(";")
+print(list3)
+
+s=input()
+list=s.split()
+n1,n2=list1
+n3=int(n1)+int(n2)
+print(f'sum of 2 numbers',n3)
+
+list1=['10','20','30']
+a,b,c=list1
+print(a,b,c,type(a),type of(b),type(c))
+list1=[10,20,30]
+a,b,c=list1
+print(int(a)+int(b)+int(c))
+x,y,z=map(int,list1)
+n=int(input())
+s=list(map(int,input().split()))
+s.sort()
+
+  
